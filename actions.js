891a0236a5036fb08df2a4851b8e5332c9919606
akinn <akinnijames@live.com>
2023-03-30 16:51:55 -0400

    dropdown

---
+++ actions.js
@@ -0,0 +1,51 @@
+var projectsSelected = false;
+var dropdown = null;
+var projects = null;
+var overProjects = false;
+var overDropdown = false;
+
+function projectsEnter() {
+    overProjects = true;
+    updateDropdown();
+}
+
+function projectsLeave() {
+    overProjects = false;
+    updateDropdown();
+}
+
+function dropdownEnter() {
+    overDropdown = true;
+    updateDropdown();
+}
+
+function dropdownLeave() {
+    overDropdown = false;
+    updateDropdown();
+}
+
+function select(option) {
+    if (option == 'projects') {
+        projectsSelected = !projectsSelected;
+    }
+    console.log('projectsSelected: ' + projectsSelected);
+}
+
+function updateDropdown() {
+    dropdown = document.getElementById("projects_dropdown");
+    projects = document.getElementById("projects");
+    dropdown.hidden = !(overProjects || overDropdown || projectsSelected);
+    if (!dropdown.hidden) {
+        projects.style.fontWeight = 'bold';
+        projects.style.backgroundColor = 'mediumpurple';
+        projects.style.color = 'white';
+    } else {
+        projects.style.fontWeight = 'normal';
+        projects.style.backgroundColor = 'lightblue';
+        projects.style.color = 'lightsteelblue';
+    }
+}
+
+function selectProjects() {
+
+}
